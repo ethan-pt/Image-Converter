2acb8ebaf5e34573d0c8823dd65108f6a1af741a
Assentt <tubbeethan@gmail.com>
2022-09-25 22:32:01 -0700

    Refactor code

--- image_converter.py
+++ image_converter.py
@@ -2,45 +2,53 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
 
-# initialize window
-root = tk.Tk()
 
-# window settings
-root.title("Image Converter")
-root.grid_columnconfigure(0, weight=2)
-root.grid_columnconfigure(3, weight=2)
-root.grid_rowconfigure(0, weight=2)
-root.grid_rowconfigure(1, weight=1)
-root.grid_rowconfigure(4, weight=2)
+class App:
+    def __init__(self, root):
+        self.root = root
 
-# button logic
-def get_img():
-    global im1
-    import_file_path = filedialog.askopenfilename()
-    im1 = Image.open(import_file_path)
+        # Tells window how to handle being resized
+        self.root.grid_columnconfigure(0, weight=2)
+        self.root.grid_columnconfigure(3, weight=2)
+        self.root.grid_rowconfigure(0, weight=2)
+        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_rowconfigure(4, weight=2)
 
-def conv_jpg():
-    rgb_im1 = im1.convert('RGB')
-    export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg')
-    rgb_im1.save(export_file_path)
+        # Define stuff
+        self.label_1 = tk.Label(root, text="Image Converter", )
+        self.button_browse = tk.Button(root, text="Browse", command=self.get_img)
+        self.button_2 = tk.Button(root, text="Convert to PNG", command=self.conv_png)
+        self.button_3 = tk.Button(root, text="Convert to JPG", command=self.conv_jpg)
 
-def conv_png():
-    export_file_path = filedialog.asksaveasfilename(defaultextension='.png')
-    im1.save(export_file_path)
+        # Display stuff
+        self.label_1.grid(row=0, column=1, columnspan=2, pady=10)
+        self.button_browse.grid(row=2, column=1, columnspan=2, pady=20)
+        self.button_2.grid(row=3, column=1, padx=5, pady=5)
+        self.button_3.grid(row=3, column=2, padx=5, pady=5)
 
-# define/display label
-label_1 = tk.Label(root, text="Image Converter", )
-label_1.grid(row=0, column=1, columnspan=2, pady=10)
+    # button logic
+    def get_img(self):
+        global im1
+        import_file_path = filedialog.askopenfilename()
+        im1 = Image.open(import_file_path)
 
-# define/display buttons
-button_browse = tk.Button(root, text="Browse", command=get_img)
-button_2 = tk.Button(root, text="Convert to PNG", command=conv_png)
-button_3 = tk.Button(root, text="Convert to JPG", command=conv_jpg)
+    def conv_jpg(self):
+        rgb_im1 = im1.convert('RGB')
+        export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg')
+        rgb_im1.save(export_file_path)
 
-button_browse.grid(row=2, column=1, columnspan=2, pady=20)
-button_2.grid(row=3, column=1, padx=5, pady=5)
-button_3.grid(row=3, column=2, padx=5, pady=5)
+    def conv_png(self):
+        export_file_path = filedialog.asksaveasfilename(defaultextension='.png')
+        im1.save(export_file_path)
 
 
-# window loop
-root.mainloop()
+def main():
+    root = tk.Tk()
+    root.title('Image Converter')
+
+    window = App(root)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
